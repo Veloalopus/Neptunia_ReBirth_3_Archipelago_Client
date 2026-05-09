57f72b4a8a2d90a2376b4d2fcd1728fdb8df0ad5
BlackSoulKnight <44965005+BlackSoulKnight@users.noreply.github.com>
2026-05-10 00:46:41 +0200

    + Better Combo Struct + removed comments

--- src/Neptunia 3 Data/Character.cs
+++ src/Neptunia 3 Data/Character.cs
@@ -8,12 +8,17 @@ using System.Threading.Tasks;
 
 namespace Nep3ArchipelagoClient.src.Neptunia_3_Data
 {
+
     public struct ComboAttack
     {
-        public short CharacterId;
-        public short AttackId;
+        public short RushCharacterId;
+        public short RushAttackId;
+        public short BreakCharacterId;
+        public short BreakAttackId;
+        public short PowerCharacterId;
+        public short PowerAttackId;
     }
-    [InlineArray(15)]
+    [InlineArray(5)]
     public struct ComboAttackEntry
     {
         ComboAttack entry;
@@ -22,44 +27,41 @@ namespace Nep3ArchipelagoClient.src.Neptunia_3_Data
     [StructLayout(LayoutKind.Explicit)]
     public unsafe struct Character
     {
-
-        //comments -> offset
         [FieldOffset(0x0)]
-        public byte CurrentForm; //0
+        public byte CurrentForm;
         [FieldOffset(0x8)]
-        public byte Cha; //8 DO NOT CHANGE
+        public byte Cha;
         [FieldOffset(4)]
-        public int Exp; //4
+        public int Exp; 
         [FieldOffset(12)]
-        public fixed byte CharacterName[32]; //12 maybe to many character last char need to be 0x00 utf8
+        public fixed byte CharacterName[32];
         [FieldOffset(68)]
-        public int CurrentHP; //68
+        public int CurrentHP;
         [FieldOffset(76)]
-        public int CurrentSP; //76
+        public int CurrentSP;
         [FieldOffset(176)]
-        public int Weapon; //176
+        public int Weapon;
         [FieldOffset(180)]
-        public int Armor;  //180
+        public int Armor;
         [FieldOffset(184)]
-        public int Ornament; //184
+        public int Ornament;
         [FieldOffset(188)]
-        public int ClothingBody; //188
+        public int ClothingBody;
         [FieldOffset(192)]
-        public int ClothingHead; //192
+        public int ClothingHead;
         [FieldOffset(196)]
-        public int CpuC; //196
+        public int CpuC;
         [FieldOffset(200)]
-        public int CpuH; //200
+        public int CpuH;
         [FieldOffset(204)]
-        public int CpuB; //204
+        public int CpuB;
         [FieldOffset(208)]
-        public int CpuS; //208
+        public int CpuS;
         [FieldOffset(212)]
-        public int CpuW; //212
+        public int CpuW;
         [FieldOffset(216)]
-        public int CpuL; //216
-        //500 skill 16 bytes?
+        public int CpuL;
         [FieldOffset(1172)]
-        public ComboAttackEntry ComboAttacks; //1172 Attacks go in order Rush -> Break -> Power -> next row
+        public ComboAttackEntry ComboAttacks;
     }
 }
